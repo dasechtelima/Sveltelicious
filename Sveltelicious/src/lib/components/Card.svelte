d721c7bb19b2cd1ea407af3cd668176b82a24218
dasechtelima <0198@htl.rennweg.at>
2024-09-16 14:57:38 +0200

    recipe card prototype

---
+++ Sveltelicious/src/lib/components/Card.svelte
@@ -0,0 +1,18 @@
+<div class="flex flex-col bg-sv-beige/60 w-[424px] h-[519px] p-2 mx-[17px] rounded-[20px]">
+	<div class="h-[199px] bg-slate-400 rounded-[15px] mb-[15px]">I am an image</div>
+	<div class="mx-2 mb-[34px]">
+		<div class="flex flex-row justify-between items-center">
+			<slot name="Name" />
+			<div class="flex flex-row">
+				<slot name="Dauer" />
+				,
+				<slot name="Schwierigkeit" />
+			</div>
+		</div>
+		<slot name="Kategorie" />
+		<p class="font-extrabold text-xl mt-[20px] mb-[5px]">Beschreibung</p>
+		<slot name="Beschreibung" />
+		<p class="font-extrabold text-xl mt-[20px] mb-[5px]">Zutaten</p>
+		<slot name="Zutaten" />
+	</div>
+</div>
