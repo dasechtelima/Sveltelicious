adc0ba6788e2c10bbe55c3a779c1f62a6e089e1f
dasechtelima <0198@htl.rennweg.at>
2024-09-16 12:08:44 +0200

    set up tailwind

--- Sveltelicious/tailwind.config.js
+++ Sveltelicious/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        "sv-white": "#F5F5F5",
+        "sv-beige": "#F5E3C8",
+        "sv-red": "#FF6F61",
+        "sv-green": "#B9DA53",
+        "sv-blue": "#84C5F5",
+      },
+      fontFamily: {
+        "sans": ["Open Sans", "sans-serif"],
+        "heading": ["Playfair", "sans-serif"],
+      },
+    }
   },
   plugins: []
 };
