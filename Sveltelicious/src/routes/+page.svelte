a4d6f2cd1c470b267dd82bec09216accfda5f6f5
dasechtelima <0198@htl.rennweg.at>
2024-09-23 16:36:38 +0200

    searchbar prototype

--- Sveltelicious/src/routes/+page.svelte
+++ Sveltelicious/src/routes/+page.svelte
@@ -1,23 +1,58 @@
 <script>
 	import { onMount } from 'svelte';
 	import Card from '$lib/components/Card.svelte';
+	import Loading from './Loading.svelte';
 
 	let recipes = [];
+	let allRecipes = [];
 
 	onMount(async () => {
 		const response = await fetch('src/lib/data/rezepte.json');
-		recipes = await response.json();
+		allRecipes = await response.json();
+		recipes = allRecipes;
 		console.log(recipes);
 	});
 
 	function getStars(difficulty) {
 		return '⭐'.repeat(difficulty);
 	}
+
+	// Searchbar
+	let search = '';
+	let timeout;
+	let searching = false;
+
+	function handle_search() {
+		searching = true;
+		if (timeout) clearTimeout(timeout);
+		timeout = setTimeout(get_recipes, 300);
+	}
+
+	function get_recipes() {
+		if (!search) {
+			reset();
+			return;
+		}
+
+		const searchTerm = search.toLowerCase();
+		recipes = allRecipes.filter(recipe => recipe.Name.toLowerCase().includes(searchTerm));
+		searching = false;
+	}
+
+	function reset() {
+		recipes = allRecipes;
+		searching = false;
+	}
 </script>
 
 <a href="/" class="font-heading text-black font-black text-6xl">
 	<span class="text-sv-red">Svelte</span>licious
 </a>
+<label for="search">Product Search</label>
+<input id="search" bind:value={search} on:input={handle_search} />
+{#if searching}
+	<Loading />
+{/if}
 <div class="flex flex-row">
 	{#each recipes as recipe}
 		<Card>
