93564054de6ad8a1604a2068201e54a1cf49ef9e
dasechtelima <0198@htl.rennweg.at>
2024-10-08 08:27:57 +0200

    added custom error page

---
+++ Sveltelicious/src/routes/+error.svelte
@@ -0,0 +1,4 @@
+<div class="pt-20 pl-20 text-sv-white flex h-screen flex-grow flex-col bg-no-repeat bg-[url('/src/lib/data/img/404pic.jpg')]">
+	<p class="font-heading text-5xl">Whoops you found the kitchen 🐀</p>
+	<a href="/" class="underline text-xl m-2">Go back where you belong...</a>
+</div>
