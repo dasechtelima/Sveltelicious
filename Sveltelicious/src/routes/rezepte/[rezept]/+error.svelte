ef88d163473a79180feb1126d345d299296ec180
Marco Janderka <75068841+Pycxtv@users.noreply.github.com>
2024-10-14 15:55:32 +0200

    Deleted README and added spaces in recipes.json

--- Sveltelicious/src/routes/rezepte/[rezept]/+error.svelte
+++ Sveltelicious/src/routes/rezepte/[rezept]/+error.svelte
@@ -1,4 +1,6 @@
-<div class="pt-20 pl-20 text-sv-white flex h-screen flex-grow flex-col bg-no-repeat bg-[url('/src/lib/data/img/404_recipes.jpg')] bg-cover">
+<div
+	class="pt-20 pl-20 text-sv-white flex h-screen flex-grow flex-col bg-no-repeat bg-[url('/src/lib/data/img/404_recipes.jpg')] bg-cover"
+>
 	<p class="font-heading text-5xl">There is no recipe for that... Ahhhh! A human!! 🐀</p>
 	<a href="/" class="underline text-xl m-2">Go back to where you belong...</a>
 </div>
